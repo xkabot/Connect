8affb4d684e7a28623c73f7b07544a2875d37769
tsg1472 <tsg1472@rit.edu>
2024-12-29 00:44:11 -0500

    Changed allowed hosts

--- connect/connect/settings.py
+++ connect/connect/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://connect-r98x.ondender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://connect-r98x.ondender.com"]
 
 
 # Application definition
